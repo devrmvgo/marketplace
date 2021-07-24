718cb5d19c7500124713f50d55fa82bb1dded84c
Luiz Felipe <felipealvesalps@gmail.com>
2021-07-24 14:30:30 -0300

    started pages

---
+++ src/pages/ProductList/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// import './styles.css';
+ 
+export default function ProductList() {
+ return (
+   <h1>Lista de produtos</h1>
+ );
+}
---
+++ src/pages/ShoppingCart/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// import './styles.css';
+ 
+export default function ShoppingCart() {
+ return (
+   <h1>Carrinho de Compras</h1>
+ );
+}
